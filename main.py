a599b3a274d070490911dde40e7eb579b6931590
Lifelimit <norisz234@gmail.com>
2024-09-21 05:04:22 +0300

    Add each character count function to main.py

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 def main():
-    file_path = "books/frankenstein.txt"
+    file_path = "github.com/Lifelimit/bookbot/books/frankenstein.txt" # github.com/Lifelimit/bookbot/books/frankenstein.txt
     text = get_book_text(file_path)
     print(text)
 
     word_count = count_words(text)
     print(f"\nThe book contains {word_count} words.")
 
+    char_count = count_characters(text)
+    print("\nCharacter counts:")
+    for char, count in char_count.items():
+        print(f"{char}: {count}")
+
+
 def get_book_text(path):
     with open(path) as f:
-       return f.read()
+       file_contents = f.read()
+       return file_contents
     
 def count_words(text):
     words = text.split()
     word_count = len(words)
     return word_count
+
+def count_characters(text):
+    lowered_characters = text.lower()
+    char_counts = {} # Initialize an empty dictionary to store character counts
+    for character in lowered_characters:
+        if character in char_counts:
+            char_counts[character] += 1 # If the character is already in the dictionary, increment its count 
+        else:
+            char_counts[character] = 1 # If it's a new character, add it to the dictionary with a count of 1
+    return char_counts
+
 
 main()
