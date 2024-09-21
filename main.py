da0312c5092282efde093d3e2819920369c47046
Lifelimit <norisz234@gmail.com>
2024-09-21 06:10:06 +0300

    Add generate report to main.py

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+import sys
+
+def generate_report(file_path, word_count, char_count):
+    report = f"""
+--- Begin report of {file_path} ---
+{word_count} words found in the document\n
+"""
+    for char, count in sorted(char_count.items(), key=lambda item: item[1], reverse=True):
+        report += f"The '{char}' character was found {count} times \n"
+    
+    report  += "--- End report ---\n"
+    return report
+   
 def main():
-    file_path = "books/frankenstein.txt" # github.com/Lifelimit/bookbot/books/frankenstein.txt
+    file_path = "books/frankenstein.txt"   # "books/frankenstein.txt" # github.com/Lifelimit/bookbot/books/frankenstein.txt
     text = get_book_text(file_path)
-    print(text)
-
-    word_count = count_words(text)
-    print(f"\nThe book contains {word_count} words.")
-
     char_count = count_characters(text)
-    print("\nCharacter counts in alphabetical order:")
-    for char, count in sorted(char_count.items()):
-        print(f"{char}: {count}")
-
+    word_count = count_words(text)
+    report = generate_report(file_path, word_count, char_count)
+    print(text)
+    print(report)
 
 def get_book_text(path):
     with open(path) as f:
@@ -26,11 +34,11 @@ def count_characters(text):
     lowered_characters = text.lower()
     char_counts = {} # Initialize an empty dictionary to store character counts
     for character in lowered_characters:
-        if character in char_counts:
-            char_counts[character] += 1 # If the character is already in the dictionary, increment its count 
-        else:
-            char_counts[character] = 1 # If it's a new character, add it to the dictionary with a count of 1
+        if character.isalpha():
+            if character in char_counts:
+                char_counts[character] += 1 # If the character is already in the dictionary, increment its count 
+            else:
+                char_counts[character] = 1 # If it's a new character, add it to the dictionary with a count of 1
     return char_counts
-
 
 main()
